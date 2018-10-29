01e7bbe86674eb461d0e565b1b0c9c3a3d14988f
William Mathewson <neanias@users.noreply.github.com>
2018-10-29 17:22:21 +0000

    Make shellcheck changes
    Use up-to-date syntax for `seq` and wrap the openssl args in quotes to enforce
    string interpolation

--- create_keys.sh
+++ create_keys.sh
@@ -9,13 +9,12 @@ else
     DIR=.
 fi
 
-for i in `seq 1 10`; do
+for i in $(seq 1 10); do
     uuid=$(uuidgen)
 
     pub=es256_$uuid.pem
     priv=es256_$uuid-priv.pem
 
-    openssl ecparam -name secp256k1 -genkey -noout -out $DIR/keys/$priv 
-    openssl ec -in $DIR/keys/$priv -pubout -out $DIR/keys/$pub
-
+    openssl ecparam -name secp256k1 -genkey -noout -out "$DIR/keys/$priv"
+    openssl ec -in "$DIR/keys/$priv" -pubout -out "$DIR/keys/$pub"
 done
